cb0ffb047442f6c73fc50a8255cce9869b0ba21b
Pratiksha Indurkar <pratikshaindurkar2015@gmail.com>
2023-12-26 12:32:06 +0530

    Add files via upload

---
+++ Node/restaurantApi/apidocument.md
@@ -0,0 +1,102 @@
+
+## Page 1
+
+List of city /locations (GET)
+
+http://localhost:4000/locations
+
+List of QuickSearch / MealTypes (GET)
+
+http://localhost:4000/quickSearch
+
+List of restaurant (GET)
+
+http://localhost:4000/restaurants
+
+restaurants wrt city (GET)
+
+http://localhost:4000/restaurants?state_id=1
+
+## Page 2
+
+restaurants wrt mealType (GET)
+
+http://localhost:4000/restaurants?mealId=1
+
+restaurants wrt cuisineId and mealId (GET)
+
+http://localhost:4000/filter/5?cuisineId=3
+
+restaurants wrt cost and mealId (GET)
+
+http://localhost:4000/filter/1?lcost=500&hcost=1000
+
+restaurants wrt cuisineId and cost (GET)
+
+http://localhost:4000/filter/2?cuisineId=1&lcost=500&hcost=900
+
+sort in basis of cost (GET)
+
+http://localhost:4000/filter/1?lcost=200&hcost=500&sort=-1
+
+# Page 3
+
+Details of restaurant (GET)
+
+http://localhost:4000/details/1
+
+Menu of restaurant (GET)
+
+http://localhost:4000/menu/2
+
+http methods meaning
+GET -> READ
+POST -> ADDING/INSERT
+PUT -> UPDATE
+DELETE -> DELETE
+
+crud - create, read, update, delete
+
+# Page 4
+
+Menu Details (POST)
+
+http://localhost:4000/menuItem
+
+input => [1,2,3]
+
+PlaceOrder (POST)
+
+{
+
+        "orderId": 1,
+        "name": "peter",
+        "email": "peter@gmail.com",
+        "address": "Home 5",
+        "phone": 7435434155,
+        "cost": 577,
+        "restName": "Domino's Pizza",
+        "menuItem": [1,2,3]
+    }
+
+## Page 5
+
+List of orders
+
+http://localhost:4000/orders
+
+order wrt email
+
+http://localhost:4000/orders?email=peter@gmail.com
+
+delete order wrt id
+
+http://localhost:4000/deleteOrder/1
+
+update payment details (PUT)
+
+{
+"status":"Delivered",
+"bank_name":"HDFC",
+"date":"12-23-23"
+}
---
+++ Node/restaurantApi/index.js
@@ -0,0 +1,210 @@
+const express = require('express')
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+const PORT = 4000
+
+const app = express()
+
+app.use(express.json())//Inbuilt Middleware
+const MONGO_URL = 'mongodb://127.0.0.1:27017';
+// 'mongodb://localhost:27017';
+let db;
+//http => get, post, put, delete
+
+// req => what you send to server
+// res => what you receive from server
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Restaurant App😀')
+})
+
+//get locations
+
+app.get('/locations', (req, res) => {
+    db.collection("locations").find().toArray((err, result) => {
+        if (err) throw err;
+        res.send(result)
+    })
+
+})
+
+//get mealType
+
+app.get('/quickSearch', (req, res) => {
+    db.collection("mealType").find().toArray((err, result) => {
+        if (err) throw err;
+        res.send(result)
+    })
+
+})
+
+
+//get restaurant data 
+
+app.get('/restaurants', (req, res) => {
+    let query = {}
+    let stateId = +req.query.state_id
+    let mealId = +req.query.mealId
+    if (stateId) {
+        query = { state_id: stateId }
+    }
+    else if (mealId) {
+        query = { "mealTypes.mealtype_id": mealId }
+    }
+
+    db.collection("restaurants").find(query).toArray((err, result) => {
+        if (err) throw err;
+        res.send(result)
+    })
+
+})
+
+//filter 
+
+
+app.get('/filter/:mealId', (req, res) => {
+    let query = {}
+    let mealId = +req.params.mealId
+    let cuisineId = +req.query.cuisineId
+    let lcost = +req.query.lcost
+    let hcost = +req.query.hcost
+    let sort = { cost: 1 }//asc
+
+    if (req.query.sort) {
+        sort = { cost: req.query.sort }
+    }
+
+    if (cuisineId) {
+        query = {
+            "mealTypes.mealtype_id": mealId,
+            "cuisines.cuisine_id": cuisineId
+        }
+    } else if (lcost && hcost) {
+        query = {
+            "mealTypes.mealtype_id": mealId,
+            $and: [{ cost: { $gt: lcost, $lt: hcost } }]
+        }
+    } else if (cuisineId && lcost && hcost) {
+        query = {
+            "mealTypes.mealtype_id": mealId,
+            "cuisines.cuisine_id": cuisineId,
+            $and: [{ cost: { $gt: lcost, $lt: hcost } }]
+        }
+    }
+
+    // -1 => desc
+    // 1 => asc
+
+    db.collection("restaurants").find(query).sort(sort).toArray((err, result) => {
+        if (err) throw err;
+        res.send(result)
+    })
+
+})
+
+//details
+
+app.get('/details/:id', (req, res) => {
+    let id = +req.params.id
+
+    db.collection("restaurants").find({ restaurant_id: id }).toArray((err, result) => {
+        if (err) throw err;
+        res.send(result)
+    })
+
+})
+
+//menu of restaurant
+
+app.get('/menu/:id', (req, res) => {
+    let id = +req.params.id
+
+    db.collection("menu").find({ restaurant_id: id }).toArray((err, result) => {
+        if (err) throw err;
+        res.send(result)
+    })
+
+})
+
+//Menu Details
+
+// express.json() => Inbuilt middleware
+app.post('/menuItem', (req, res) => {
+    if (Array.isArray(req.body)) {
+        db.collection("menu").find({ menu_id: { $in: req.body } })
+            .toArray((err, result) => {
+                if (err) throw err;
+                res.send(result)
+            })
+    } else {
+        res.send("Invalid Input")
+    }
+})
+
+
+//placeorder
+app.post('/placeOrder', (req, res) => {
+    console.log(req.body)
+
+    db.collection("orders").insertOne(req.body, (err, result) => {
+        if (err) throw err;
+        res.send("Order Placed")
+    })
+})
+
+//getOrders
+
+app.get('/orders', (req, res) => {
+    let query = {}
+    let email = req.query.email
+    if (email) {
+        query = { email }
+    }
+    db.collection("orders").find(query).toArray((err, result) => {
+        res.send(result)
+    })
+})
+
+
+//delete order
+
+app.delete('/deleteOrder/:id', (req, res) => {
+    let oid = +req.params.id
+    db.collection("orders").deleteOne({ orderId: oid }, (err, result) => {
+        if (err) throw err;
+        res.send("Order deleted successfully")
+    })
+})
+
+//update payment details
+
+
+app.put('/updateOrder/:id', (req, res) => {
+    let oid = +req.params.id
+    db.collection("orders").updateOne({ orderId: oid },
+        {
+            $set: {
+                status: req.body.status,
+                bank_name: req.body.bank_name,
+                date: req.body.date,
+            }
+        },
+        (err, result) => {
+            if (err) throw err;
+            res.send("Order updated successfully")
+        })
+})
+
+
+
+
+
+
+//Mongo connection 
+
+MongoClient.connect(MONGO_URL, (err, client) => {
+    console.log("MongoDB is connected")
+    if (err) console.log("Error while connecting to Mongo")
+    db = client.db("ed-sep-intern")
+    app.listen(PORT, () => console.log("Server connected on the PORT", PORT))
+})
